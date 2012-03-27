0f799d642a9c875b866b00fe6febf252c1fe9c54
Ruslan Zaynetdinov <ruslan@skifflemedia.ru>
2012-03-27 22:50:13 +0600

    map update

--- page.tpl.php
+++ page.tpl.php
@@ -29,6 +29,10 @@
 <div id="site-subheader-wrapper" class="clearfix">
 <div id="site-subheader" class="container-16 clearfix">
     
+    <div id="front_map">
+        <iframe width="428" height="297" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=tallinn,+estonia&amp;aq=&amp;sll=37.0625,-95.677068&amp;sspn=55.455479,114.169922&amp;vpsrc=6&amp;ie=UTF8&amp;hq=&amp;hnear=Tallinn,+Harju+County,+Estonia&amp;t=m&amp;ll=59.439839,24.753742&amp;spn=0.025922,0.073299&amp;z=13&amp;output=embed"></iframe>
+    </div>
+
   <?php if ($page['highlighted']): ?>
     <div id="highlighted" class="<?php print ns('grid-16', $page['header'], 8); ?>">
       <?php print render($page['highlighted']); ?>
@@ -37,9 +41,6 @@
 
   <?php if ($page['header']): ?>
     <div id="header-region" class="region <?php print ns('grid-16', $page['highlighted'], 8); ?> clearfix">
-        <div id="front_map">
-            <iframe width="428" height="297" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=tallinn,+estonia&amp;aq=&amp;sll=37.0625,-95.677068&amp;sspn=55.455479,114.169922&amp;vpsrc=6&amp;ie=UTF8&amp;hq=&amp;hnear=Tallinn,+Harju+County,+Estonia&amp;t=m&amp;ll=59.439839,24.753742&amp;spn=0.025922,0.073299&amp;z=13&amp;output=embed"></iframe>
-        </div>
       <?php print render($page['header']); ?>
     </div>
   <?php endif; ?>
